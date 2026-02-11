05036582ae45049da56290b808388b1bc54f9f45
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-11 20:17:47 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/index.dart
@@ -0,0 +1,6 @@
+// Export pages
+export '/tasks/tasks_widget.dart' show TasksWidget;
+export '/login/login_widget.dart' show LoginWidget;
+export '/onboarding/onboarding_widget.dart' show OnboardingWidget;
+export '/task_details/task_details_widget.dart' show TaskDetailsWidget;
+export '/completed_page/completed_page_widget.dart' show CompletedPageWidget;
